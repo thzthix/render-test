d6dd9b9dfddb077cf4854e53548a03c6c895b024
[thzthix] <seohas0428@naver.com>
2023-07-18 02:33:43 +0900

    added error handler middleware

--- mongo.js
+++ mongo.js
@@ -25,7 +25,13 @@ const phoneSchema = new mongoose.Schema({
   name: String,
   number: String,
 });
-
+phoneSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 const Persons = mongoose.model("Person", phoneSchema);
 
 // if (process.argv.length > 3) {
